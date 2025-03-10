7674e756352e55005408cb3c8e84e63571aacea1
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-03-10 12:22:31 -0600

    Use const instead of let for iterating over Memory.creeps in cleanCreepMemory method

--- MemoryManager.js
+++ MemoryManager.js
@@ -1,7 +1,6 @@
-
 class MemoryManager {
     static cleanCreepMemory() {
-        for (let name in Memory.creeps) {
+        for (const name in Memory.creeps) {
             if (!Game.creeps[name]) {
                 delete Memory.creeps[name];
                 console.log('Clearing non-existing creep memory:', name);
