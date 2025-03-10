827a92653718ec63446df2ba7d004ae87b4ad506
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-03-10 12:26:37 -0600

    Define path styles for upgrading and harvesting in RoleUpgrader for improved readability

--- RoleUpgrader.js
+++ RoleUpgrader.js
@@ -1,5 +1,8 @@
 const CreepRole = require("./CreepRole");
 
+const PATH_STYLE_UPGRADE = { stroke: '#ffffff' };
+const PATH_STYLE_HARVEST = { stroke: '#ffaa00' };
+
 class RoleUpgrader extends CreepRole {
     constructor(creep) {
         super(creep);
@@ -15,12 +18,12 @@ class RoleUpgrader extends CreepRole {
 
         if (this.creep.memory.working) {
             if (this.creep.upgradeController(this.creep.room.controller) == ERR_NOT_IN_RANGE) {
-                this.creep.moveTo(this.creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+                this.creep.moveTo(this.creep.room.controller, { visualizePathStyle: PATH_STYLE_UPGRADE });
             }
         } else {
             const source = this.creep.pos.findClosestByPath(FIND_SOURCES);
             if (source && this.creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                this.creep.moveTo(source, { visualizePathStyle: { stroke: '#ffaa00' } });
+                this.creep.moveTo(source, { visualizePathStyle: PATH_STYLE_HARVEST });
             }
         }
     }
