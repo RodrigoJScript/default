829883145420127bd694c41723d6e9c7c94d6202
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-03-10 12:23:23 -0600

    Remove console log for clearing non-existing creep memory in MemoryManager

--- MemoryManager.js
+++ MemoryManager.js
@@ -3,7 +3,6 @@ class MemoryManager {
         for (const name in Memory.creeps) {
             if (!Game.creeps[name]) {
                 delete Memory.creeps[name];
-                console.log('Clearing non-existing creep memory:', name);
             }
         }
     }
