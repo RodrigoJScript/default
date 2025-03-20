05a9c5bc3ce1ee722609b566d5f25132e6d32c95
RodrigoJScript <rodrigo777.rjp@gmail.com>
2025-03-20 12:20:57 -0600

    Adjust desired upgrader count in SpawnManager from 2 to 1; optimize StructureTower repair logic for damaged structures.

--- StructureTower.js
+++ StructureTower.js
@@ -4,42 +4,42 @@ class StructureTower {
             return;
         }
 
-        var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-        if (closestHostile && tower.pos.inRangeTo(closestHostile, 20)) {
+        const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
             tower.attack(closestHostile);
+            return;
+        }
+
+        if (Game.time % 50 >= 40) {
+            const damagedStructures = tower.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (
+                        structure.structureType == STRUCTURE_RAMPART &&
+                        structure.hits < structure.hitsMax
+                    );
+                }
+            });
+
+            damagedStructures.sort((a, b) => a.hits - b.hits);
+
+            if (damagedStructures.length > 0) {
+                const currentIndex = Game.time % damagedStructures.length;
+                tower.repair(damagedStructures[currentIndex]);
+            }
         } else {
-            if (Game.time % 50 >= 40) {
-                var damagedStructures = tower.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (
-                            (structure.structureType == STRUCTURE_RAMPART) &&
-                            structure.hits < structure.hitsMax
-                        );
-                    }
-                });
-
-                damagedStructures.sort((a, b) => a.hits - b.hits);
-
-                if (damagedStructures.length > 0) {
-                    var currentIndex = Game.time % damagedStructures.length;
-                    tower.repair(damagedStructures[currentIndex]);
+            const otherDamagedStructures = tower.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (
+                        (structure.structureType == STRUCTURE_ROAD ||
+                            structure.structureType == STRUCTURE_CONTAINER) &&
+                        structure.hits < structure.hitsMax
+                    );
                 }
-            } else {
-                var otherDamagedStructures = tower.room.find(FIND_STRUCTURES, {
-                    filter: (structure) => {
-                        return (
-                            (structure.structureType == STRUCTURE_ROAD ||
-                                structure.structureType == STRUCTURE_CONTAINER /*||
-                    structure.structureType == STRUCTURE_RAMPART*/) &&
-                            structure.hits < structure.hitsMax
-                        );
-                    }
-                });
+            });
 
-                if (otherDamagedStructures.length > 0) {
-                    otherDamagedStructures.sort((a, b) => a.hits - b.hits);
-                    tower.repair(otherDamagedStructures[0]);
-                }
+            if (otherDamagedStructures.length > 0) {
+                otherDamagedStructures.sort((a, b) => a.hits - b.hits);
+                tower.repair(otherDamagedStructures[0]);
             }
         }
     }
